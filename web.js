37783b67402a2a06ad41a2c88d2de9edc55d0988
Raja Emmadi <remmadi8@gmail.com>
2013-07-11 10:15:54 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
+var fs = requires('fs');
 var app = express.createServer(express.logger());
+var myBuff = new Buffer(200);
+
+
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  data.copy(myBuff);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('This is test');
 });
 
 var port = process.env.PORT || 5000;
