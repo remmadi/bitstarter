c354dd4ec02afc4d6ad49fe9b579ae1f893156cc
Raja Emmadi <remmadi8@gmail.com>
2013-07-11 10:29:04 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,13 +1,5 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
-var myBuff = new Buffer(200);
-
-
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  data.copy(myBuff);
-});
 
 app.get('/', function(request, response) {
   response.send('This is test');
