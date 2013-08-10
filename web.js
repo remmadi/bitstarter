ac195c47029c8d653c37a350f1e57b134e889110
Raja Emmadi <remmadi8@gmail.com>
2013-08-10 16:53:59 +0000

    Edited index.html

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var app = express.createServer(express.logger());
 var fs = require('fs');
 
 var mbuff = fs.readFileSync('index.html');
 
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
+app.use(express.static(__dirname + '/img'));
   response.send(mbuff.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
